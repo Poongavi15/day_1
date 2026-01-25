f7e91876b62115949017d87b82b1bf2e0634b8ab
Poongavi15 <poongavijothi@gmail.com>
2026-01-25 21:49:34 +0530

    practing datatypes

---
+++ practice/types.ts
@@ -0,0 +1,32 @@
+"use strict";
+let user:string="poongavi";
+console.log(user)
+let marks: number= 50;
+console.log(marks)
+let isavailable: boolean=true;
+let res=isavailable=false;
+console.log(res);
+let data:null=null;
+console.log(data);
+let page:undefined=undefined
+console.log(page)
+let bigN:bigint=988674333340006567808766n;
+console.log(bigN)
+let mark:number[]=[10,20,30,40]
+console.log(mark);
+let skills:string[]=['js','ts']
+console.log(skills);
+let person:[string,string,number]=['poovie','chennai',55]
+console.log(person);
+var vehicle1 = {
+    noofwheels: 2,
+    typeofvehicle: "scooty",
+    price: 50000
+};
+var vehicle2 = {
+    noofwheels: 4,
+    typeofvehicle: "car",
+    price: 500000
+};
+console.log(vehicle1);
+console.log(vehicle2);
